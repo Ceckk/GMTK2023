7a4d6a0d05bee28bdc6fd66251e8a823e3c2d36a
Ceck <andrea.cecovig@gmail.com>
2023-07-08 15:23:55 +0700

    * flappy bird version

--- Assets/Scripts/ScreenMove.cs
+++ Assets/Scripts/ScreenMove.cs
@@ -2,22 +2,48 @@ using UnityEngine;
 
 public class ScreenMove : MonoBehaviour
 {
-    public float power = 10;
+    public float pushPower = 10;
+    public float pullPower = 10;
+    public float cooldown = 0.2f;
     private Vector3 _mousePos;
+    private Quaternion _target;
+    private float _power;
+    private float _time = 0;
 
     void Update()
     {
+        // if (Input.GetMouseButtonDown(0))
+        // {
+        //     _mousePos = Input.mousePosition;
+        // }
+        // if (Input.GetMouseButton(0))
+        // {
+        //     var delta = _mousePos - Input.mousePosition;
+        //     _mousePos = Input.mousePosition;
+        //     transform.Rotate(0, 0, delta.y * power * Time.deltaTime);
+        // }
+
+        _time -= Time.deltaTime;
+        if (_time <= 0)
+        {
+            _target = Quaternion.identity;
+            _power = pullPower;
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
-            _mousePos = Input.mousePosition;
+            _target = Quaternion.Euler(0, 0, 90);
+            _time = cooldown;
+            _power = pushPower;
         }
-        if (Input.GetMouseButton(0))
+        
+        if (Input.GetMouseButtonDown(1))
         {
-            var delta = _mousePos - Input.mousePosition;
-            _mousePos = Input.mousePosition;
-            transform.Rotate(0, 0, delta.y * power * Time.deltaTime);
+            _target = Quaternion.Euler(0, 0, -90);
+            _time = cooldown;
+            _power = pushPower;
         }
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, _target, _power * Time.deltaTime);
     }
-
-
 }
