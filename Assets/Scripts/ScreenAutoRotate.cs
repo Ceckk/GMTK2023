e28493bba53db711824fe381e802aa9f47011318
Ceck <andrea.cecovig@gmail.com>
2023-07-09 10:47:51 +0700

    * auto rotate limits

--- Assets/Scripts/ScreenAutoRotate.cs
+++ Assets/Scripts/ScreenAutoRotate.cs
@@ -5,16 +5,33 @@ public class ScreenAutoRotate : MonoBehaviour
     public float powerUp = 10;
     public float powerDown = 1;
     public Transform _rotationTarget;
+    private float _totalRotation;
+    private float rotMax = 30;
+    private float rotMin = -30;
 
     void Update()
     {
+        var rot = 0f;
         if (Input.GetMouseButton(0))
         {
-            transform.RotateAround(_rotationTarget.position, Vector3.forward, powerUp * Time.deltaTime);
+            rot = powerUp * Time.deltaTime;
         }
         else
         {
-            transform.RotateAround(_rotationTarget.position, Vector3.forward, -powerDown * Time.deltaTime);
+            rot = -powerDown * Time.deltaTime;
         }
+
+        if (_totalRotation + rot > rotMax)
+        {
+            rot = rotMax - _totalRotation;
+        }
+        else if (_totalRotation + rot < rotMin)
+        {
+            rot = rotMin - _totalRotation;
+        }
+
+        transform.RotateAround(_rotationTarget.position, Vector3.forward, rot);
+        _totalRotation += rot;
+        Debug.Log(_totalRotation);
     }
 }
